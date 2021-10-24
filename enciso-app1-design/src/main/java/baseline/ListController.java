100f8b9e05916d39de72bb3a0264142b34aa46e7
Cristiam Enciso <encisocristiam@gmil.com>
2021-10-24 19:29:35 -0400

    small update

--- enciso-app1-design/src/main/java/baseline/ListController.java
+++ enciso-app1-design/src/main/java/baseline/ListController.java
@@ -1,9 +1,8 @@
 package baseline;
 /*
- *  UCF COP3330 Summer 2021 Application Assignment 1 Solution
+ *  UCF COP3330 Fall 2021 Application Assignment 1 Solution
  *  Copyright 2021 Cristiam Enciso
  */
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
--- enciso-app1-design/src/main/java/baseline/ListManager.java
+++ enciso-app1-design/src/main/java/baseline/ListManager.java
@@ -1,7 +1,7 @@
 package baseline;
 
 /*
- *  UCF COP3330 Summer 2021 Application Assignment 1 Solution
+ *  UCF COP3330 Fall 2021 Application Assignment 1 Solution
  *  Copyright 2021 Cristiam Enciso
  */
 import javafx.application.Application;
